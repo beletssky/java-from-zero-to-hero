2dd7f0b80d504bc16a7dcd61214bb23e1cc11af3
AhmaD <pachvash@gmail.com>
2026-05-14 22:37:14 +0200

    try_1

--- java-module-1/src/main/java/com/course/tasks/Task13.java
+++ java-module-1/src/main/java/com/course/tasks/Task13.java
@@ -8,7 +8,23 @@ public class Task13 {
      * Примеры: isTriangle(3, 4, 5) == true; isTriangle(1, 1, 5) == false.
      */
     public static boolean isTriangle(int a, int b, int c) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй isTriangle");
+        if (a<=0){
+            return false;
+        }
+        if (b<=0){
+            return false;
+        }
+        if (c<=0){
+            return false;
+        }
+        if (a+b>c){
+            if (a+c>b){
+                if (c+b>a){
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 }

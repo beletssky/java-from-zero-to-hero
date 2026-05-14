a72ceff0d1fe7ad772d7095f200dc497a5fa5842
AhmaD <pachvash@gmail.com>
2026-05-14 16:10:12 +0200

    try_1

--- java-module-1/src/main/java/com/course/tasks/Task01.java
+++ java-module-1/src/main/java/com/course/tasks/Task01.java
@@ -2,14 +2,8 @@ package com.course.tasks;
 
 public class Task01 {
 
-    /**
-     * !!!!создай новую ветку в гите!!!!!
-     * <p>
-     * Возвращает строку "Hello, World!".
-     * Пример: helloWorld() == "Hello, World!".
-     */
     public static String helloWorld() {
-        // TODO: реализуй тест
-        throw new UnsupportedOperationException("TODO: реализуй helloWorld");
+        String helloWorld = "Hello, World!";
+        return helloWorld;
     }
 }

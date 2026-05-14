81eecdf3b56cbfbf9aaa11c3784f3e2858949a3b
AhmaD <pachvash@gmail.com>
2026-05-14 23:22:39 +0200

    try_1

--- java-module-1/src/main/java/com/course/tasks/Task14.java
+++ java-module-1/src/main/java/com/course/tasks/Task14.java
@@ -24,7 +24,7 @@ public class Task14 {
             case 7:
                  return "Вс";
             default:
-                 return"?";
+                 return "?";
 
 
         }
--- java-module-1/src/main/java/com/course/tasks/Task15.java
+++ java-module-1/src/main/java/com/course/tasks/Task15.java
@@ -8,7 +8,36 @@ public class Task15 {
      * Реализовать через switch.
      */
     public static int daysInMonth(int month, boolean leap) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй daysInMonth");
+        switch (month){
+            case 1:
+                return 31;
+            case 2:
+                if (leap==true){
+                    return 29;
+                }
+                return 28;
+            case 3:
+                return 31;
+            case 4:
+                return 30;
+            case 5:
+                return 31;
+            case 6:
+                return 30;
+            case 7:
+                return 31;
+            case 8:
+                return 31;
+            case 9:
+                return 30;
+            case 10:
+                return 31;
+            case 11:
+                return 30;
+            case 12:
+                return 31;
+            default:
+                return -1;
     }
+}
 }

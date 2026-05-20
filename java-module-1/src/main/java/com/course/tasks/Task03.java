14be8de9fc5ef8875bc9c8a54fe14301563578d2
Lordeyser <lordeyser@gmail.com>
2026-05-20 21:28:42 +0200

    Nikitosik tretiy raz kommit

--- java-module-1/src/main/java/com/course/tasks/Task03.java
+++ java-module-1/src/main/java/com/course/tasks/Task03.java
@@ -1,11 +1,6 @@
 package com.course.tasks;
 
 public class Task03 {
-
-    /**
-     * Возвращает среднее арифметическое трёх целых чисел в виде double.
-     * Примеры: averageOfThree(1, 2, 3) == 2.0; averageOfThree(1, 1, 2) ≈ 1.333...
-     */
     public static double averageOfThree(int a, int b, int c) {
         int d = a + b + c;
         return d;

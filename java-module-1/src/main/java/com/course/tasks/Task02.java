b33ce775442cb9b3206443669c19bd962d5df769
Lordeyser <lordeyser@gmail.com>
2026-05-20 20:38:06 +0200

    Nikitosik wtoroy raz kommit

--- java-module-1/src/main/java/com/course/tasks/Task02.java
+++ java-module-1/src/main/java/com/course/tasks/Task02.java
@@ -7,7 +7,6 @@ public class Task02 {
      * Примеры: sumTwo(2, 3) == 5; sumTwo(-1, 1) == 0.
      */
     public static int sumTwo(int a, int b) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй sumTwo");
+        return a + b;
     }
 }

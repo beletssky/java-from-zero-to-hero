c82a01672b7a6026089276b73292453859935991
Lordeyser <lordeyser@gmail.com>
2026-05-22 17:34:02 +0200

    Nikitosik Task 16

--- java-module-1/src/main/java/com/course/tasks/Task16.java
+++ java-module-1/src/main/java/com/course/tasks/Task16.java
@@ -1,15 +1,28 @@
 package com.course.tasks;
 
 public class Task16 {
-
-    /**
-     * Простой калькулятор. op ∈ {'+', '-', '*', '/'}.
-     * При op == '/' и b == 0 — ArithmeticException("div by zero").
-     * Любой другой op — IllegalArgumentException("unknown op").
-     * Реализовать через switch.
-     */
     public static double calculate(double a, double b, char op) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй calculate");
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException("div by zero");
+                }
+                return a/b;
+            default:
+                throw new IllegalArgumentException("unknown op")
+        }
     }
 }
+
+/**
+ * Простой калькулятор. op ∈ {'+', '-', '*', '/'}.
+ * При op == '/' и b == 0 — ArithmeticException("div by zero").
+ * Любой другой op — IllegalArgumentException("unknown op").
+ * Реализовать через switch.
+ */

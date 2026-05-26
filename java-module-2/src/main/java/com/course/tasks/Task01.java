be5b235b5e5974acd610eb90271937d0d1390d5e
Lordeyser <lordeyser@gmail.com>
2026-05-27 00:14:27 +0200

    Nikitosik Tasks 1-11

--- java-module-2/src/main/java/com/course/tasks/Task01.java
+++ java-module-2/src/main/java/com/course/tasks/Task01.java
@@ -1,13 +1,12 @@
 package com.course.tasks;
 
 public class Task01 {
-
     /**
      * Возвращает квадрат числа n (n * n).
      * Примеры: square(5) == 25; square(-3) == 9; square(0) == 0.
      */
     public static int square(int n) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй square");
+        int result = n * n;
+        return result;
     }
 }

a5b48767ac794a94111ea092339cd0957441a220
Lordeyser <lordeyser@gmail.com>
2026-05-29 20:42:34 +0200

    Nikitosik Tasks 1-12, 15 - 19, 21 - 24, 27 - 30

--- java-module-2/src/main/java/com/course/tasks/Task19.java
+++ java-module-2/src/main/java/com/course/tasks/Task19.java
@@ -1,14 +1,22 @@
 package com.course.tasks;
 
 public class Task19 {
-
     /**
      * Возвращает строку, перевёрнутую посимвольно. Использовать StringBuilder или цикл.
      * Для {@code null} — IllegalArgumentException.
      * Примеры: reverseString("hello") == "olleh"; reverseString("") == "".
      */
     public static String reverseString(String s) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй reverseString");
+        if (s == null) {
+            throw new IllegalArgumentException("Кола Зиро а ты далбае");
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
     }
 }

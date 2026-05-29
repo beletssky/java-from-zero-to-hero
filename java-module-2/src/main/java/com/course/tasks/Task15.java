d694742427f77d28e9acede9f880e3e68e0c4863
Lordeyser <lordeyser@gmail.com>
2026-05-29 20:42:34 +0200

    Nikitosik Tasks 1-12, 15 - 19, 21 - 24, 27 - 30

--- java-module-2/src/main/java/com/course/tasks/Task15.java
+++ java-module-2/src/main/java/com/course/tasks/Task15.java
@@ -1,14 +1,25 @@
 package com.course.tasks;
 
 public class Task15 {
-
     /**
-     * Возвращает новый массив только из положительных (&gt; 0) элементов.
+     * Возвращает новый массив только из положительных (> 0) элементов.
      * Порядок сохраняется.
      * Примеры: filterPositive({-1,2,-3,4,0}) == {2,4}; filterPositive({}) == {}.
      */
     public static int[] filterPositive(int[] a) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй filterPositive");
+        int count = 0;
+        for (int x : a) {
+            if (x > 0) count++;
+
+        }
+        int [] result = new int[count];
+        int j = 0;
+        for (int x : a) {
+            if (x > 0) {
+                result[j] = x;
+                j++;
+            }
+        }
+        return result;
     }
 }

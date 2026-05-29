e95a67ead3b83332775e6f20fc3a4e9ed1aa1314
Lordeyser <lordeyser@gmail.com>
2026-05-29 20:53:08 +0200

    1-11

--- java-module-2/src/main/java/com/course/tasks/Task08.java
+++ java-module-2/src/main/java/com/course/tasks/Task08.java
@@ -1,14 +1,21 @@
 package com.course.tasks;
 
 public class Task08 {
-
     /**
      * Возвращает минимальный элемент массива.
      * Для пустого массива или {@code null} — IllegalArgumentException.
      * Примеры: minArray({3,1,4,1,5}) == 1.
      */
     public static int minArray(int[] a) {
-        // TODO: реализуй
-        throw new UnsupportedOperationException("TODO: реализуй minArray");
+        if (a == null || a.length == 0) {
+            throw new IllegalArgumentException("Г");
+        }
+        int min = a[0];
+        for (int i : a) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
     }
 }
